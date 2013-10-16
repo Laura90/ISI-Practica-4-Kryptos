0f7cd231796d04c086b2b025563b0b23bfcffa1e
Javii91 <javifh91@gmail.com>
2013-10-16 16:36:25 +0200

    Mejorada parabola y final 09

--- 09-enemies/spec/FireBallSpec.js
+++ 09-enemies/spec/FireBallSpec.js
@@ -24,29 +24,31 @@ it("Fireball.step()", function(){
 		
 		fire1 = new FireBall(200,420,1);
 		fire2 = new FireBall(250,480,-1);
+		fire3 = new FireBall(550,580,-1);
 
 		var dummyFireb = function () {
   			this.remove= function(obj) {}	
   		};
 
-  		fire1.board = new dummyFireb();
-  		fire2.board = new dummyFireb();
+  		fire3.board = new dummyFireb();
+
   		
-  		spyOn(fire1.board, "remove")
-  		spyOn(fire2.board, "remove")
+  		spyOn(fire3.board, "remove");
   		
-		var dt = 1;
+		var dt = 0.1;
 		fire1.step(dt);
 		expect(fire1.x).toBe(200 - fire1.w/2 +fire1.vx * dt);
-		fire1.step(dt);
-		expect(fire1.bajada).toBe(true);
-		expect(fire1.board.remove).toHaveBeenCalled();
+		expect(fire1.y).toBe(420 - fire1.h + (fire1.vy -50) * dt);
+		expect(fire1.vy).toBe(-800);
+		
 		
 		fire2.step(dt);
 		expect(fire2.x).toBe(250 - fire2.w/2 + fire2.vx * dt);
-		fire2.step(dt);
-		expect(fire2.bajada).toBe(true);
-		expect(fire2.board.remove).toHaveBeenCalled();
+		expect(fire2.y).toBe(480 - fire2.h + (fire2.vy-50) * dt);
+		expect(fire2.vy).toBe(-800);
+		
+		fire3.step(1);
+		expect(fire3.board.remove).toHaveBeenCalled();
 		
   	
  		
@@ -56,7 +58,7 @@ it("Fireball.step()", function(){
 
 	it("fireBall.draw()",function() {	
 	
-		ball= new FireBall(10,10);
+		ball= new FireBall(10,10,1);
 		
 		spyOn(SpriteSheet, "draw");
 		
